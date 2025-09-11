975ff1735954728169110e102b4509b6827148a0
Nightk75 <vincentktran@cpp.edu>
2025-09-11 15:25:14 -0700

    Fixing CrashCourseCode code and Making Lab 2 files

---
+++ CrashCourseCode.cpp
@@ -0,0 +1,15 @@
+/** isGreatherThan
+* @brief returns whether x is greater than y
+*
+* @param x : (double)
+* @param y : (int)
+* @return true : x is greater than y
+* @return false : x is less than or equal to y
+*/
+#include <iostream>
+using namespace std;
+
+bool isGreatherThan(double x, int y) {
+    return x > y;
+}
+
