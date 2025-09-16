615d57c48f4e93cda68df08d2602f87e2e58d761
Nightk75 <vincentktran@cpp.edu>
2025-09-16 14:26:06 -0700

    Finished and fixed printMemVec() and incVecBy10 functions

--- Lab 2/vector.cpp
+++ Lab 2/vector.cpp
@@ -5,10 +5,13 @@ using namespace std;
 /**
  * @brief  prints the elements in the vector and their memory locations
  * 
- * @param ... 
+ * @param vector 
  */
-void printMemVec(const int vec[], int size){
-    printf("Vector - Each int is worth %i ")
+void printMemVec(const int vector[], int size){
+    printf("Vector - Each int is worth %lu bytes\n", sizeof(vector[0]));
+    for(int i = 0; i < size; i++){
+        printf("Value %i at Memory Location: %p\n", vector[i], vector + i);
+    }
 
 }
 
@@ -17,14 +20,21 @@ void printMemVec(const int vec[], int size){
  * 
  * @param ... 
  */
-void incVecBy10(...){
-
+void incVecBy10(int vector[], int size){
+for(int i = 0; i < size; i++){
+    vector[i] += 10;
+}
 }
 
 int main(){
+    const int SIZE = 5;
+    int vector[SIZE];
+    for(int i = 0; i < SIZE; i++){
+        vector[i] = 100 + i;
+    }
     
     printf("Before Increment----------\n");
-    printMemVec()
+    printMemVec(vector, SIZE);
 
     printf("After Increment----------\n");
 
