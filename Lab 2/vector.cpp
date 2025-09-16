0663669506bab14b267e72632bd1f4ae79876578
Nightk75 <vincentktran@cpp.edu>
2025-09-16 13:19:30 -0700

    Input base code for vector.cpp

---
+++ Lab 2/vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * @brief  prints the elements in the vector and their memory locations
+ * 
+ * @param ... 
+ */
+void printMemVec(const int vec[], int size){
+    printf("Vector - Each int is worth %i ")
+
+}
+
+/**
+ * @brief 
+ * 
+ * @param ... 
+ */
+void incVecBy10(...){
+
+}
+
+int main(){
+    
+    printf("Before Increment----------\n");
+    printMemVec()
+
+    printf("After Increment----------\n");
+
+    printf("After Pop----------\n");
+
+    printf("After Push----------\n");
+
+    return 0;
+}
