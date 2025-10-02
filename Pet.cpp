cda3392f3318f8e78c257b633939556ae59fe349
Nightk75 <vincentktran@cpp.edu>
2025-10-02 12:56:03 -0700

    Update Pet.cpp

---
+++ Pet.cpp
@@ -0,0 +1,38 @@
+#include "Pet.h"
+#include <iostream>
+#include <string>
+
+Pet :: Pet() {
+    name = ""
+    age = 
+    owner = 
+    isHouseTrained = true
+
+
+}
+
+Pet :: Pet(string name, inte age, string occupation, bool isHouseTrained)
+    this-> name = name;
+    this-> age = age;
+    this-> owner = owner;
+    this-> isHouseTrained;
+
+void Pet :: updateAge(int new_age) {
+    age = new_age;
+}
+
+string Pet :: updateName() const {
+    return name;
+}
+
+int Pet :: updateAge() const {
+    return age;
+}
+
+string Pet :: getOwner() const {
+    return owner;
+}
+
+bool Pet :: setHouseTrained() const {
+    return 
+}
