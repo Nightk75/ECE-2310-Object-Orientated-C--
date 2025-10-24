985429b840e536974f7bd0cc75128e0fbe596632
Nightk75 <vincentktran@cpp.edu>
2025-10-23 20:25:56 -0700

    Finished Charmander.h outline

---
+++ Lab 5/Charmander.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include "Pokemon.h"
+using namespace std;
+
+class Charmander : public Pokemon{
+    public:
+        //Constructors
+        Charmander();
+        Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s);
+        //Mutators
+        void speak()/*override*/;
+        void printStats()/*overrid*/;
+
+    private:
+        vector<string> skills;
+        /* name, hp, attack, defense */
+};
+#endif
