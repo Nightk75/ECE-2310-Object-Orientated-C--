743f54fb3b212f50bfeba3645432083f684a2d53
Nightk75 <vincentktran@cpp.edu>
2025-11-06 13:09:05 -0800

    Update Game.h

--- Lab 4/Game.h
+++ Lab 4/Game.h
@@ -1,12 +1,19 @@
 #ifndef Game_h
 #define Game_h
+#include "RPG.h"
 
 
 class Game{
     public:
         Game();
         void generatePlayers(int n);
-    
+        int selectPlayer();
+        void endRound(winner: RPG*, loser: RPG*, loserIndex: int)
+        void battleRound()
+        void gameLoop()
+        void printFinalResults()
+        ~Game()
+
     private:
         vector<RPG*> players;
         set<int> live_players;
