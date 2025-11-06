33d1b18123aa9499a13decca1307747346ce4488
Nightk75 <vincentktran@cpp.edu>
2025-11-06 15:36:20 -0800

    Update Game.cpp

--- Lab 4/Game.cpp
+++ Lab 4/Game.cpp
@@ -48,22 +48,17 @@ if(playerIndex1 == playerIndex2){
 RPG* player1 = players[playerIndex1];
 RPG* player2 = players[playerIndex2];
 
-RPG* winner;
-RPG* loser;
-int loserIndex = playerIndex2;
+
 while(player1 -> isAlive() && player2 -> isAlive()){
     player1 -> attack(player2);
     player2 -> attack(player1);
-    loserIndex = playerIndex2;
 }
 
 if(player1 -> isAlive()){
     winner = player1;
-    loserIndex = playerIndex1;
 } else {
     if(player2 -> isAlive()){
     winner = player2;
-    loserIndex = playerIndex2;
     }
 }
 endRound(winner, loser, loserIndex);
